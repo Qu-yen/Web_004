440214e0c78ab994bf66efd5a98cee94c1f8ce00
Qu-yen <dieuquyen31082002@gmail.com>
2024-08-01 04:07:05 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>-------pases all-------</h1>
+    <h2>
+      <nuxt-link to="/pages01">next pages01</nuxt-link>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/pages01/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>-------pases 1-------</h1>
+        <h2>
+            <nuxt-link to="/">back pages all</nuxt-link>
+        </h2>
+    </div>
+</template>
